6bb79a2e9f5ec350642ecc46ef8cce84ff724c74
Ian McWilliams <ian.mcwilliams@f3mmedia.co.uk>
2018-10-01 19:26:42 +0100

    move ingres methods to new module

--- run.rb
+++ run.rb
@@ -1,52 +1,6 @@
 require_relative 'google_spreadsheet_wrapper'
 require 'awesome_print'
 
-def current_balance_from_sheets(hash_spreadsheet)
-  hash_account_sheets = hash_spreadsheet.select { |k, _| k =~ /^\d{4}$/ }
-  hash_account_sheets.reverse_each do |_, current_sheet|
-    current_balance = current_balance_from_sheet(current_sheet)
-    return current_balance if current_balance
-  end
-  nil
-end
-
-def current_balance_from_sheet(current_sheet)
-  balance_cells = current_sheet[:cells].select { |_, v| v == { value: 'BALANCE' } }
-  return nil if balance_cells.empty?
-  max_balance_index = balance_cells.map { |k, _| k[/\d+$/].to_i }.max
-  bank_balance_column_index = column_index(current_sheet, 'bank_balance')
-  cell_key = "#{bank_balance_column_index}#{max_balance_index}"
-  current_sheet[:cells][cell_key][:value].gsub(/[\D+.]/, '').to_f/100
-end
-
-def column_index(current_sheet, column_header)
-  cell_keys = current_sheet[:cells].select { |_, v| v[:value] == column_header }.keys
-  raise("no instance of column header 'bank_balance' found") if cell_keys.count < 1
-  raise("more than one instance of 'bank_balance' found") if cell_keys.count > 1
-  cell_keys[0][/^\D+/]
-end
-
-def table_sheet_to_hash_array(hash_sheet)
-  headers = hash_sheet[:cells].select { |k, _| k =~ /^\D+1$/ }
-  ap headers
-  row_max = hash_sheet[:cells].keys.map { |k| k[/\d+/].to_i }.max
-  ap row_max
-  (2..row_max).map do |row_id|
-    current_row = hash_sheet[:cells].select { |k, _| k =~ /^\D#{Regexp.quote(row_id.to_s)}$/ }
-    # ap current_row
-    current_hash = headers.each_with_object({}) do |(cell_key, cell), h|
-      target_cell_key = "#{cell_key[/^\D+/]}#{row_id}"
-      h[cell[:value]] = current_row[target_cell_key][:value]
-    end
-    current_hash
-  end
-end
-
-def transactions_from_sheet(hash_spreadsheet)
-  regular_costs_sheet = hash_spreadsheet['regular_costs']
-  table_sheet_to_hash_array(regular_costs_sheet)
-end
-
 
 def load_sheet
   hash_spreadsheet = GoogleSpreadsheetWrapper.google_spreadsheet_as_hash_spreadsheet('f3m_finance')
