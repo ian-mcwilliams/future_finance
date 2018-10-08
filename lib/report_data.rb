5ef1d692a02c9c6ce0f6fe58e53434d0a6614aea
Ian McWilliams <ian.mcwilliams@f3mmedia.co.uk>
2018-10-08 22:29:12 +0100

    move ordering and balancing out to after transactions extended

--- lib/report_data.rb
+++ lib/report_data.rb
@@ -16,29 +16,43 @@ module ReportData
 
   def self.hash_months(transactions, opening_balance)
     months = transactions.map { |transaction| transaction[:date].strftime('%y%m') }.uniq
-    current_balance = (opening_balance * 100).to_i
-    minimum_balance = current_balance
     draft_hash_months = months.each_with_object({}) do |month, h|
-      h[month] = { opening_balance: current_balance.to_f / 100 }
-      raw_month_transactions = transactions.select { |t| t[:date].strftime('%y%m') == month }
-      processed_month_transactions = []
-      raw_month_transactions.map do |t|
-        current_balance += t[:amount].delete(',').delete('.').to_i
-        minimum_balance = current_balance if current_balance < minimum_balance
-        processed_month_transaction = { balance: current_balance.to_f / 100 }.merge(t)
-        processed_month_transactions << processed_month_transaction
-      end
-      h[month][:transactions] = processed_month_transactions
-      h[month][:closing_balance] = current_balance.to_f / 100
-      h[month][:minimum_balance] = minimum_balance.to_f / 100
+      month_transactions = transactions.select { |t| t[:date].strftime('%y%m') == month }
+      h[month] = { transactions: month_transactions }
     end
+    positioned_transactions(draft_hash_months)
+    balanced_transactions(draft_hash_months, opening_balance)
+    draft_hash_months
+  end
+
+  def self.positioned_transactions(draft_hash_months)
     draft_hash_months.values.each do |month|
       last_transactions = month[:transactions].select { |transaction| transaction[:last] }
       month[:transactions].delete_if { |transaction| transaction[:last] }
       month[:transactions].concat(last_transactions)
       month[:transactions].each { |transaction| transaction.delete(:last) }
+      last_transactions = month[:transactions].select { |transaction| transaction[:first] }
+      month[:transactions].delete_if { |transaction| transaction[:first] }
+      month[:transactions] = last_transactions + month[:transactions]
+      month[:transactions].each { |transaction| transaction.delete(:first) }
     end
-    draft_hash_months
+  end
+
+  def self.balanced_transactions(draft_hash_months, start_balance)
+    current_balance = nil
+    draft_hash_months.values.each do |month|
+      opening_balance = current_balance || (start_balance * 100).to_i
+      current_balance = opening_balance
+      minimum_balance = current_balance
+      month[:transactions].each do |transaction|
+        current_balance += (transaction[:amount].delete(',').to_f * 100).to_i
+        minimum_balance = current_balance if current_balance < minimum_balance
+        transaction[:balance] = current_balance.to_f / 100
+      end
+      month[:opening_balance] = opening_balance.to_f / 100
+      month[:closing_balance] = current_balance.to_f / 100
+      month[:minimum_balance] = minimum_balance.to_f / 100
+    end
   end
 
   def self.all_extracted_transactions(raw_transactions)
@@ -106,7 +120,8 @@ module ReportData
       date: current_date,
       description: transaction['description'],
       amount: transaction['amount'],
-      last: transaction['payment_date'] == 'last'
+      last: transaction['payment_date'] == 'last',
+      first: transaction['position'] == 'first'
     }
   end
 
