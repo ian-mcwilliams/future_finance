0554e4097a8633f49b314cc11edbe196eba0f9db
ian-mcwilliams <ian.mcwilliams@f3mmedia.com>
2019-01-26 21:20:37 +0000

    use sheet_name instead of target

--- lib/excel_spreadsheet_wrapper.rb
+++ lib/excel_spreadsheet_wrapper.rb
@@ -1,18 +1,18 @@
 require 'simple_xlsx_reader'
 
 module ExcelSpreadsheetWrapper
-  def self.hash_spreadsheet(filename, target)
-    raw_spreadsheet = excel_spreadsheet(filename)
-    excel_spreadsheet_to_hash_spreadsheet(raw_spreadsheet, target)
+  def self.hash_spreadsheet(params)
+    raw_spreadsheet = excel_spreadsheet(params[:filename])
+    excel_spreadsheet_to_hash_spreadsheet(raw_spreadsheet, params[:sheet_name])
   end
 
   def self.excel_spreadsheet(filename)
     SimpleXlsxReader.open("artefacts/#{filename}.xlsx")
   end
 
-  def self.excel_spreadsheet_to_hash_spreadsheet(raw_spreadsheet, target)
+  def self.excel_spreadsheet_to_hash_spreadsheet(raw_spreadsheet, sheet_name)
     hash_spreadsheet = {}
-    planner_sheet = raw_spreadsheet.sheets.select { |sheet| sheet.name == target }[0]
+    planner_sheet = raw_spreadsheet.sheets.select { |sheet| sheet.name == sheet_name }[0]
     planner_sheet.rows.each_with_index do |row, row_i|
       row.each_with_index do |cell, col_i|
         value = cell.is_a?(Date) ? cell.strftime('%d-%m-%Y') : cell
