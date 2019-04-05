56963f060038fbb8cbbd22756d09eb80fef7747b
ian-mcwilliams <ian.mcwilliams@f3mmedia.com>
2019-04-05 23:01:29 +0100

    add full report and selection arg

--- lib/generate_report.rb
+++ lib/generate_report.rb
@@ -29,4 +29,23 @@ module GenerateReport
     ExcelReportGenerator.output_report(combined_report_object, save_filepath)
   end
 
+  def self.full_report(company_report_data, personal_report_data, income_report_data, save_filepath)
+    Dir.mkdir('reports') unless File.directory?('reports')
+    company = ExcelReportGenerator.report_object(company_report_data)
+    personal = ExcelReportGenerator.report_object(personal_report_data)
+    income = ExcelReportGenerator.report_object(income_report_data)
+    combined_report_object = {
+      'company_months_summary' => company.delete('months_summary'),
+      'company_all_transactions' => company.delete('all_months'),
+      'personal_months_summary' => personal.delete('months_summary'),
+      'personal_sheets_summary' => personal.delete('sheets_summary'),
+      'personal_payee_summary' => personal.delete('payee_summary'),
+      'personal_all_transactions' => personal.delete('all_months'),
+      'personal_income_summary' => income['months_summary'],
+      'personal_income_transactions' => income['all_months']
+    }
+    # combined_report_object.merge!(personal)
+    ExcelReportGenerator.output_report(combined_report_object, save_filepath)
+  end
+
 end
--- run.rb
+++ run.rb
@@ -4,25 +4,38 @@ require_relative 'lib/excel_spreadsheet_wrapper'
 require_relative 'lib/excel_report_generator'
 require_relative 'lib/generate_report'
 require_relative 'lib/google_spreadsheet_wrapper'
-
-# params = Config.run_parameters
-
-# source = {
-#   excel: ExcelSpreadsheetWrapper,
-#   google: GoogleSpreadsheetWrapper,
-# }[params[:source].to_sym]
-#
-# destinations = [BasicReportGenerator, ExcelReportGenerator]
-#
-# GenerateReport.generate_report(params, source, destinations)
-
-
 require_relative 'lib/report_data'
 
+def run_basic
+  params = Config.run_parameters
 
-params = Config.run_parameters('personal')
-personal_report_data = ReportData.report_data(ExcelSpreadsheetWrapper, params)
-params = Config.run_parameters('income')
-income_report_data = ReportData.report_data(ExcelSpreadsheetWrapper, params)
-GenerateReport.personal_report(personal_report_data, income_report_data, params[:save_filepath])
+  source = {
+    excel: ExcelSpreadsheetWrapper,
+    google: GoogleSpreadsheetWrapper,
+  }[params[:source].to_sym]
 
+  destinations = [BasicReportGenerator, ExcelReportGenerator]
+
+  GenerateReport.generate_report(params, source, destinations)
+end
+
+
+
+def run_alt
+  params = Config.run_parameters('f3m')
+  company_report_data = ReportData.report_data(ExcelSpreadsheetWrapper, params)
+  params = Config.run_parameters('personal')
+  personal_report_data = ReportData.report_data(ExcelSpreadsheetWrapper, params)
+  params = Config.run_parameters('income')
+  income_report_data = ReportData.report_data(ExcelSpreadsheetWrapper, params)
+  save_filepath = "reports/full_#{DateTime.now.strftime('%y%m%d%H%M%S')}.xlsx"
+  GenerateReport.full_report(company_report_data, personal_report_data, income_report_data, save_filepath)
+end
+
+args = ARGV.map { |arg| arg }
+
+if args.empty?
+  run_basic
+elsif args[0] == 'alt'
+  run_alt
+end
